0aa102f0d287dc81eda1174d9fc0b9cb6496f071
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-13 15:09:04 +0530

    change

--- Lecture_3/Index.js
+++ Lecture_3/Index.js
@@ -1,2 +1,25 @@
 var argv = require('minimist')(process.argv.slice(2));
-console.log(argv);
+
+let val1 = argv.a;
+let val2 = argv.b;
+let op = argv.o;
+switch (op) {
+    case '+':
+    console.log(val1 + val2)
+    break;
+
+    case '-':
+    console.log(val1 - val2)
+    break;
+
+    case '*':
+    console.log(val1 * val2)
+    break;
+    case '/':
+    console.log(val1 / val2)
+    break;
+
+    default:
+    console.log('invalid operator')
+    break;
+}
