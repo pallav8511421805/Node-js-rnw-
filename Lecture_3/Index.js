c6a640555122cb4a9c15cff25c8059861183efa0
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-15 14:21:28 +0530

    change

--- Lecture_3/Index.js
+++ Lecture_3/Index.js
@@ -3,21 +3,33 @@ var argv = require('minimist')(process.argv.slice(2));
 let val1 = argv.a;
 let val2 = argv.b;
 let op = argv.o;
+const sum = (x,y) =>{
+return x+y
+}
+const sub = (x,y) =>{
+    return x-y
+}
+const mul = (x,y) =>{
+    return x*y
+}
+const div = (x,y) =>{
+    return x/y
+}
 switch (op) {
     case '+':
-    console.log(val1 + val2)
+    console.log("Addition : ",sum(val1,val2))
     break;
 
     case '-':
-    console.log(val1 - val2)
+    console.log('subtraction',sub(val1,val2))
     break;
 
     case '*':
-    console.log(val1 * val2)
+    console.log('Multiplication : ',mul(val1,val2))
     break;
     
     case '/':
-    console.log(val1 / val2)
+    console.log('Division : ',div(val1,val2))
     break;
 
     default:
