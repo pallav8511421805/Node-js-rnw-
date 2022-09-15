0ed2c538ec6f3ad02d566a823b2f37cbf25049e6
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-15 14:35:41 +0530

    change

--- Lecture_4/Index.js
+++ Lecture_4/Index.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
-const filedata = fs.createReadStream('Text.txt')
+const filedata = fs.createReadStream('Text1.txt')
 filedata.on('data',(data)=>{
     console.log('File : ' ,data.toString())
+})
+filedata.on('error',(e)=>{
+    console.log('File ERROR : ' ,e)
 })
 console.log('Program End')
