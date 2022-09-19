84dee18474341936b16ca3ef74cfb28cb7236ab6
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-19 16:07:36 +0530

    new

---
+++ Pr1/index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const fs = require("fs");
+function Homepage(req, res) {
+  fs.readFile("Home.html", function (err, data) {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(data);
+    res.end();
+  });
+}
+function Aboutpage(req, res) {
+  fs.readFile("About.html", function (err, data) {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(data);
+    res.end();
+  });
+}
+let api = http.createServer(function (req, res) {
+  switch (req.url) {
+    case "/about":
+      Aboutpage(req, res);
+      break;
+
+    default:
+      Homepage(req, res);
+      break;
+  }
+});
+api.listen(3001);
