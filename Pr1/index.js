21bf71e8875574c4097b051ddbc91cec54bf46ca
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-20 08:52:40 +0530

    change

--- Pr1/index.js
+++ Pr1/index.js
@@ -1,29 +1,22 @@
 const http = require("http");
 const fs = require("fs");
-function Homepage(req, res) {
-  fs.readFile("index.html", function (err, data) {
-    res.writeHead(200, {
-      "Content-Type": "text/html",
-    });
-    res.write(data);
-    res.end();
-  });
-}
-function Aboutpage(req, res) {
-  fs.readFile("About.html", function (err, data) {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(data);
-    res.end();
+function defaultpage(req, res) {
+  fs.readFile(__dirname + req.url, function (err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.write(err.toString());
+      res.end();
+    } else {
+      res.writeHead(200);
+      res.write(data);
+      res.end();
+    }
   });
 }
 let api = http.createServer(function (req, res) {
   switch (req.url) {
-    case "/about":
-      Aboutpage(req, res);
-      break;
-
     default:
-      Homepage(req, res);
+      defaultpage(req, res);
       break;
   }
 });
