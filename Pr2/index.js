5cf96400fe1a53afc83c163da319ee8648e4ca60
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-26 16:38:49 +0530

    chage

--- Pr2/index.js
+++ Pr2/index.js
@@ -1,73 +1,74 @@
-const http = require("http");
-const fs = require("fs");
-function workpage(req, res) {
-  fs.readFile("work.html", function (err, data) {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(data);
-    res.end();
-  });
-}
-function work1page(req, res) {
-  fs.readFile("work1.html", function (err, data) {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(data);
-    res.end();
-  });
-}
-function homepage(req, res) {
-  fs.readFile("index.html", function (err, data) {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(data);
-    res.end();
-  });
-}
-function teampage(req, res) {
-  fs.readFile("team.html", function (err, data) {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(data);
-    res.end();
-  });
-}
-function blogpage(req, res) {
-  fs.readFile("blog.html", function (err, data) {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(data);
-    res.end();
-  });
-}
-function defaultpage(req, res) {
-  fs.readFile(__dirname + req.url, function (err, data) {
-    if (err) {
-      res.writeHead(404);
-      res.write(err.toString());
-      res.end();
-    } else {
-      res.writeHead(200);
-      res.write(data);
-      res.end();
-    }
-  });
-}
-let app = http.createServer(function (req, res) {
-  switch (req.url) {
-    case "/":
-      homepage(req, res);
-      break;
-    case "/w":
-      workpage(req, res);
-      break;
-    case "/t":
-      teampage(req, res);
-      break;
-    case "/w1":
-      work1page(req, res);
-      break;
-    case "/b":
-      blogpage(req, res);
-      break;
-    default:
-      defaultpage(req, res);
-      break;
-  }
-});
-app.listen(8000);
+// const http = require("http");
+// const fs = require("fs");
+// function workpage(req, res) {
+//   fs.readFile("work.html", function (err, data) {
+//     res.writeHead(200, { "content-type": "text/html" });
+//     res.write(data);
+//     res.end();
+//   });
+// }
+// function work1page(req, res) {
+//   fs.readFile("work1.html", function (err, data) {
+//     res.writeHead(200, { "content-type": "text/html" });
+//     res.write(data);
+//     res.end();
+//   });
+// }
+// function homepage(req, res) {
+//   fs.readFile("index.html", function (err, data) {
+//     res.writeHead(200, { "content-type": "text/html" });
+//     res.write(data);
+//     res.end();
+//   });
+// }
+// function teampage(req, res) {
+//   fs.readFile("team.html", function (err, data) {
+//     res.writeHead(200, { "content-type": "text/html" });
+//     res.write(data);
+//     res.end();
+//   });
+// }
+// function blogpage(req, res) {
+//   fs.readFile("blog.html", function (err, data) {
+//     res.writeHead(200, { "content-type": "text/html" });
+//     res.write(data);
+//     res.end();
+//   });
+// }
+// function defaultpage(req, res) {
+//   fs.readFile(__dirname + req.url, function (err, data) {
+//     if (err) {
+//       res.writeHead(404);
+//       res.write(err.toString());
+//       res.end();
+//     } else {
+//       res.writeHead(200);
+//       res.write(data);
+//       res.end();
+//     }
+//   });
+// }
+// let app = http.createServer(function (req, res) {
+//   switch (req.url) {
+//     case "/":
+//       homepage(req, res);
+//       break;
+//     case "/w":
+//       workpage(req, res);
+//       break;
+//     case "/t":
+//       teampage(req, res);
+//       break;
+//     case "/w1":
+//       work1page(req, res);
+//       break;
+//     case "/b":
+//       blogpage(req, res);
+//       break;
+//     default:
+//       defaultpage(req, res);
+//       break;
+//   }
+// });
+// app.listen(8000);
+
