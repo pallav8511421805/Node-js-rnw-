0045af7dd309f9da6b9e9c505a1c2ff7242f6891
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-30 07:07:59 +0530

    change

--- Lecture_4_a/index.js
+++ Lecture_4_a/index.js
@@ -1,28 +1,7 @@
 const http = require("http");
 const fs = require("fs");
-function Homepage(req, res) {
-  fs.readFile("Home.html", function (err, data) {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(data);
-    res.end();
-  });
-}
-function Aboutpage(req, res) {
-  fs.readFile("About.html", function (err, data) {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(data);
-    res.end();
-  });
-}
-let api = http.createServer(function (req, res) {
-  switch (req.url) {
-    case "/about":
-      Aboutpage(req, res);
-      break;
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
 
-    default:
-      Homepage(req, res);
-      break;
-  }
-});
 api.listen(3001);
