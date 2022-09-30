7fd61604cf5431df6f46819a7ce75721bd4db05b
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-30 08:15:40 +0530

    change

--- Pr2/index.js
+++ Pr2/index.js
@@ -11,19 +11,19 @@ app.get("/", (req, res) => {
   res.render("index", { title: title });
 });
 
-app.get("/w1", (req, res) => {
+app.get("/work1.html", (req, res) => {
   res.render("work1", { title: title });
 });
 
-app.get("/w", (req, res) => {
+app.get("/work.html", (req, res) => {
   res.render("work", { title: title });
 });
 
-app.get("/b", (req, res) => {
+app.get("/blog.html", (req, res) => {
   res.render("blog", { title: title });
 });
 
-app.get("/t", (req, res) => {
+app.get("/team.html", (req, res) => {
   res.render("team", { title: title });
 });
 
