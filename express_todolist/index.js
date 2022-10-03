2820979e7a08f0d58e7e1a10b4fb9cf1702ebfeb
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-03 16:42:06 +0530

    change

--- express_todolist/index.js
+++ express_todolist/index.js
@@ -1,48 +1,38 @@
 const express = require("express");
-const fs = require("fs");
+
+const bodyParser = require("body-parser");
+
 const app = express();
+
 const port = 3000;
+
 const options = {
   index: false,
 };
+
+const NewI = [];
+
 app.use(express.static("public", options));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  fs.readFile("data.json", function (e, d) {
-    let output = "";
-    if (d) {
-      const task = JSON.parse(d);
-      for (let t of task) {
-        output += `<tr>`;
-        output += `<td>${t.name}</td>`;
-        output += `<td>${t.age}</td>`;
-        output += `<td>${t.status}</td>`;
-        output += `</tr>`;
-      }
-    }
-    res.render("index", { opt: output });
-  });
+  res.render("index", { newitem: NewI });
 });
 
-app.get("/create", (req, res) => {
-  if (req.method == "POST") {
-    var body = [];
-    req.on("data", function (data) {
-      body += data;
-    });
-    req.on("end", function () {
-      var post = qs.parse(body);
-      fs.readFile("data.json", (error, data) => {
-        var output = [];
-        if (!error) {
-          output = JSON.parse(data);
-        }
-        output.push(post);
-        fs.writeFile("data.json", JSON.stringify(output), (e, data) => {});
-      });
-    });
-    res.redirect("/");
-  }
+app.post("/create", (req, res) => {
+  let name = req.body.name;
+  let age = req.body.age;
+  let status = req.body.status;
+  const data = {
+    Name: name,
+    Age: age,
+    Status: status,
+  };
+  NewI.push(data);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
