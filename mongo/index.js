1cd97a18235c94c5201be77ac199a6b55116c952
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-10 17:40:37 +0530

    chasnge

--- mongo/index.js
+++ mongo/index.js
@@ -1,1 +1,11 @@
-const express =  
+const express = require('express')
+const app = express()
+const port = 3000;
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+app.listen(port,()=>{
+    console.log(port)
+})
