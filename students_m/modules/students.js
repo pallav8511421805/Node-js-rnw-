8c53bb213d7066c46f964fd4320a1178f6b9e865
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-13 18:39:47 +0530

    change

--- students_m/modules/students.js
+++ students_m/modules/students.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
-const Students = mongoose.model('Students', { name: String,age:Number,fname:String,mname:String,fphone : Number });
+const Students = mongoose.model('Students', { name: String,age:Number,fname:String,mname:String,fphone : Number ,gender : String});
 module.exports = Students;
