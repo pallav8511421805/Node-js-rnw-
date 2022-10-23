721cc9a977153e72987da41b416d878806c3248f
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-23 08:00:51 +0530

    change

--- students_m/index.js
+++ students_m/index.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const appRoute = require('./routes')
+const session = require('express-session')
 const port = 3000;
 
 mongoose.connect('mongodb://localhost:27017/school');
@@ -12,6 +13,8 @@ app.set("view engine", "ejs")
 const options = {
     index: false,
 }
+
+app.use(session({ secret: 'this is secret key.', cookie: { maxAge: 60000 } }))
 
 app.get("/", async function (req, res) {
     res.render('home')
