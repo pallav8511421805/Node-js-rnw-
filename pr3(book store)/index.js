2e1a501cb2a64e65965335e9572c111d395480d8
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-23 08:26:58 +0530

    change

---
+++ pr3(book store)/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const appRoute = require('./routes')
+const port = 3000;
+
+mongoose.connect('mongodb://localhost:27017/school');
+
+app.set("view engine", "ejs")
+
+const options = {
+    index: false,
+}
+
+app.use(express.static('public', options))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(appRoute)
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}/`)
+})
