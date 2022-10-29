372f3c391bfa76978e8f57c74109fbbcec818957
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-29 12:46:38 +0530

    change

--- pr3(book store)/models/book.js
+++ pr3(book store)/models/book.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
-const books = mongoose.model('Books', { title: String, aname: String, rate: Number, bookg: String, price: Number, pname: String, date: Date });
+const books = mongoose.model('Books', { title: String, aname: String, rate: Number, bookg: String, price: Number, pname: String, date: String });
 module.exports = books;
