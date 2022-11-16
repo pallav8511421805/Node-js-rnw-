f7c1fbbb77484b1c2113ba479642f4e66bd112ca
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-11-16 11:46:20 +0530

    change

--- passportjs/index.js
+++ passportjs/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = 3000;
+
+mongoose.connect('mongodb://localhost:27017/passport');
 
 app.listen(port, () => {
     console.log(`server start on this server : ${port}`)
