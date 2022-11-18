28a22b2b1c7c462c4fb50950ab173b9fc4644d46
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-11-18 10:20:44 +0530

    change

--- passportjs/routes/index.js
+++ passportjs/routes/index.js
@@ -4,7 +4,21 @@ const Route = express.Router();
 const authRoute = require('./auth.route')
 const ProfileRoute = require('./profile.route')
 
-Route.use('/', authRoute)
-Route.use('/profile', ProfileRoute)
+Route.use('/', (req, res, next) => {
+    const User = req.user;
+    if (User.name) {
+        res.redirect('/profile')
+    } else {
+        next();
+    }
+}, authRoute)
+Route.use('/profile', (req, res, next) => {
+    const User = req.user;
+    if (User.name) {
+        next();
+    } else {
+        res.redirect('/login')
+    }
+}, ProfileRoute)
 
 module.exports = Route;
