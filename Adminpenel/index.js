2e4b867862a4f3119dc220df771b1bfc0444234f
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-12-27 20:36:26 +0530

    change

--- Adminpenel/index.js
+++ Adminpenel/index.js
@@ -7,11 +7,10 @@ const path = require('path');
 
 app.set('view engine', 'ejs')
 
-app.set('views', [path.join(__dirname + '/src/Dashbroad', 'views'), path.join(__dirname, 'views')])
+app.set('views', [path.join(__dirname + '/src/Dashbroad', 'views'), path.join(__dirname + '/src/Profile', 'views')])
 app.use(express.static('public'))
 app.use(AppRouter)
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
-    console.log(__dirname);
 })
