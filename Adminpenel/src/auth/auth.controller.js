0789c6c8c603bbfbff11d0ee844dd8ca00aa9141
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-12-28 12:41:43 +0530

    change

--- Adminpenel/src/auth/auth.controller.js
+++ Adminpenel/src/auth/auth.controller.js
@@ -3,7 +3,16 @@ class Authcontroller {
         res.render('login')
     }
     loginpost(req, res) {
-        console.log(req.body)
+        const { email, password } = req.body;
+
+        const memail = 'pallav@gmail.com';
+        const mpass = '123456';
+
+        if (memail === email && mpass === password) {
+            res.redirect('/')
+        } else {
+            res.redirect('/auth/login')
+        }
     }
     forgetpassword(req, res) {
         res.render('forgetpass')
